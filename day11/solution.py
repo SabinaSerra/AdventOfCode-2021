0a0abb9fcc162e9c0fdf099e8fa3fa0f62099734
SabinaSerra <Sabina.serra@cygni.se>
2021-12-11 22:52:37 +0100

    Ugly solution

--- day11/solution.py
+++ day11/solution.py
@@ -1,23 +1,86 @@
 from os import environ
+from functools import reduce
+import time
+from pprint import pprint
+
+class Grid:
+    def __init__(self, input_list):
+        self.width = len(input_list[0])
+        self.height = len(input_list)
+        self.data = reduce(lambda a,b:a+b, input_list)
+        
+    def __str__(self):
+        grid = ""
+        for ind in range(0, len(self.data), self.width):
+            grid += str(self.data[ind:ind+self.width]) + "\n"
+        return grid
+
+    def get_elem(self, row, col):
+        ind = row * self.width + col
+        return self.data[ind]
 
 
-def solve_part1(input_list):
-    return
+def add_adjecent(input_list, inds):
+    if len(inds) == 0:
+        return 
+    row, col = inds[0]
+    for row_ind in range(max(row - 1, 0), min(row+2, len(input_list))):
+        for col_ind in range(max(col - 1, 0), min(col + 2, len(input_list[0]))):
+            input_list[row_ind][col_ind] += 1
+            if input_list[row_ind][col_ind] == 10:
+                inds.append((row_ind, col_ind))
+    return add_adjecent(input_list, inds[1:])
+
+
+def step(input_list):
+    inds = []
+    for row in range(0, len(input_list)):
+        for col in range(0, len(input_list[0])):
+            input_list[row][col] += 1
+            if input_list[row][col] == 10:
+                inds.append((row, col))
+    add_adjecent(input_list, inds)
+    return update_flare(input_list)
+    
+
+def update_flare(input_list):
+    counter = 0
+    for row in range(0, len(input_list)):
+        for col in range(0, len(input_list[0])):
+            if input_list[row][col] >= 10:
+                counter += 1
+                input_list[row][col] = 0
+    return counter
+
+
+def solve_part1(input_list, num_steps = 5):
+    counter = 0
+    for i in range(num_steps):
+        counter += step(input_list)
+    return counter 
 
 
 def solve_part2(input_list):
     return
 
+def parse_input(filename):
+    with open(filename) as f:
+        file_input = [[int(x) for x in list(line.strip())] for line in f.readlines()]
+    return file_input
 
 def main():
+    start = time.time()
     part = environ.get('part')
-    with open('input.txt') as f:
-        file_input = [int(x) for x in f.readlines()]
+    file_input = parse_input('input.txt')
     if part == 'part1':
-        print(solve_part1(file_input))
+        pprint(solve_part1(file_input))
     else:
         print(solve_part2(file_input))
+    end = time.time()
+    print(f"Time: {round((end - start)*1000, 4)}ms")
     
 
 if __name__ == "__main__":
     main()
+
+# first 0.6819ms
---
+++ day11/test.py
@@ -0,0 +1,82 @@
+import unittest
+import solution
+import numpy as np
+from pprint import pprint
+
+class TestSolution(unittest.TestCase):
+
+    def test_step(self):
+        problem_input = [[5,4,8,3,1,4,3,2,2,3],
+                        [2,7,4,5,8,5,4,7,1,1],
+                        [5,2,6,4,5,5,6,1,7,3],
+                        [6,1,4,1,3,3,6,1,4,6],
+                        [6,3,5,7,3,8,5,4,7,8],
+                        [4,1,6,7,5,2,4,6,4,5],
+                        [2,1,7,6,8,4,1,7,2,1],
+                        [6,8,8,2,8,8,1,1,3,4],
+                        [4,8,4,6,8,4,8,5,5,4],
+                        [5,2,8,3,7,5,1,5,2,6]]
+        expected_res1 = [[6,5,9,4,2,5,4,3,3,4],
+                        [3,8,5,6,9,6,5,8,2,2],
+                        [6,3,7,5,6,6,7,2,8,4],
+                        [7,2,5,2,4,4,7,2,5,7],
+                        [7,4,6,8,4,9,6,5,8,9],
+                        [5,2,7,8,6,3,5,7,5,6],
+                        [3,2,8,7,9,5,2,8,3,2],
+                        [7,9,9,3,9,9,2,2,4,5],
+                        [5,9,5,7,9,5,9,6,6,5],
+                        [6,3,9,4,8,6,2,6,3,7]]
+
+        expected_res2 = [[8,8,0,7,4,7,6,5,5,5],
+                        [5,0,8,9,0,8,7,0,5,4],
+                        [8,5,9,7,8,8,9,6,0,8],
+                        [8,4,8,5,7,6,9,6,0,0],
+                        [8,7,0,0,9,0,8,8,0,0],
+                        [6,6,0,0,0,8,8,9,8,9],
+                        [6,8,0,0,0,0,5,9,4,3],
+                        [0,0,0,0,0,0,7,4,5,6],
+                        [9,0,0,0,0,0,0,8,7,6],
+                        [8,7,0,0,0,0,6,8,4,8]]
+        counter = solution.step(problem_input)
+        pprint(problem_input)
+        self.assertListEqual(problem_input, expected_res1)
+        counter += solution.step(problem_input)
+        pprint(problem_input)
+        self.assertListEqual(problem_input, expected_res2)
+
+
+    def test_flashes(self):
+        problem_input = [[1,1,1,1,1],
+                        [1,9,9,9,1],
+                        [1,9,1,9,1],
+                        [1,9,9,9,1],
+                        [1,1,1,1,1]]
+
+        expected_res = [[3,4,5,4,3],
+                        [4,0,0,0,4],
+                        [5,0,0,0,5],
+                        [4,0,0,0,4],
+                        [3,4,5,4,3]]
+        counter = solution.step(problem_input)
+        pprint(problem_input)
+        self.assertListEqual(problem_input, expected_res)
+
+
+    def test_part1(self):
+        problem_input = [[5,4,8,3,1,4,3,2,2,3],
+                        [2,7,4,5,8,5,4,7,1,1],
+                        [5,2,6,4,5,5,6,1,7,3],
+                        [6,1,4,1,3,3,6,1,4,6],
+                        [6,3,5,7,3,8,5,4,7,8],
+                        [4,1,6,7,5,2,4,6,4,5],
+                        [2,1,7,6,8,4,1,7,2,1],
+                        [6,8,8,2,8,8,1,1,3,4],
+                        [4,8,4,6,8,4,8,5,5,4],
+                        [5,2,8,3,7,5,1,5,2,6]]
+        counter = solution.solve_part1(problem_input, num_steps=100)
+        print(problem_input)
+        self.assertEqual(counter, 1656)
+
+
+if __name__ == '__main__':
+    unittest.main()
