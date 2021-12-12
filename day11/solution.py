4d56b2333c2ac330905063f777aa12ab8c64240c
SabinaSerra <Sabina.serra@cygni.se>
2021-12-12 01:04:40 +0100

    Change to numpy solution

--- day11/solution.py
+++ day11/solution.py
@@ -1,67 +1,42 @@
 from os import environ
-from functools import reduce
 import time
-from pprint import pprint
-
-class Grid:
-    def __init__(self, input_list):
-        self.width = len(input_list[0])
-        self.height = len(input_list)
-        self.data = reduce(lambda a,b:a+b, input_list)
-        
-    def __str__(self):
-        grid = ""
-        for ind in range(0, len(self.data), self.width):
-            grid += str(self.data[ind:ind+self.width]) + "\n"
-        return grid
-
-    def get_elem(self, row, col):
-        ind = row * self.width + col
-        return self.data[ind]
+import numpy as np
 
 
-def add_adjecent(input_list, inds):
+def add_adjecent(grid, inds):
     if len(inds) == 0:
         return 
     row, col = inds[0]
-    for row_ind in range(max(row - 1, 0), min(row+2, len(input_list))):
-        for col_ind in range(max(col - 1, 0), min(col + 2, len(input_list[0]))):
-            input_list[row_ind][col_ind] += 1
-            if input_list[row_ind][col_ind] == 10:
-                inds.append((row_ind, col_ind))
-    return add_adjecent(input_list, inds[1:])
+    for row_ind in range(max(row - 1, 0), min(row+2, grid.shape[0])):
+        for col_ind in range(max(col - 1, 0), min(col + 2, grid.shape[1])):
+            grid[row_ind, col_ind] += 1
+            if grid[row_ind, col_ind] == 10:
+                inds.append([row_ind, col_ind])
+    return add_adjecent(grid, inds[1:])
 
+def step(grid):
+    grid += 1
+    inds = np.argwhere(grid > 9).tolist()
+    add_adjecent(grid, inds)
+    flared = np.argwhere(grid > 9)
+    grid[flared[:,0], flared[:, 1]] = 0
+    return flared.shape[0]
 
-def step(input_list):
-    inds = []
-    for row in range(0, len(input_list)):
-        for col in range(0, len(input_list[0])):
-            input_list[row][col] += 1
-            if input_list[row][col] == 10:
-                inds.append((row, col))
-    add_adjecent(input_list, inds)
-    return update_flare(input_list)
-    
-
-def update_flare(input_list):
-    counter = 0
-    for row in range(0, len(input_list)):
-        for col in range(0, len(input_list[0])):
-            if input_list[row][col] >= 10:
-                counter += 1
-                input_list[row][col] = 0
-    return counter
-
-
-def solve_part1(input_list, num_steps = 5):
+def solve_part1(grid, num_steps = 100):
     counter = 0
     for i in range(num_steps):
-        counter += step(input_list)
+        counter += step(grid)
     return counter 
 
 
-def solve_part2(input_list):
-    return
+def solve_part2(grid):
+    i = 0
+    while i < 1000:
+        i += 1
+        step(grid)
+        if np.all(grid == 0):
+            return i 
+        
 
 def parse_input(filename):
     with open(filename) as f:
@@ -69,14 +44,16 @@ def parse_input(filename):
     return file_input
 
 def main():
-    start = time.time()
     part = environ.get('part')
+    start = time.time()
     file_input = parse_input('input.txt')
+    grid = np.array(file_input)
     if part == 'part1':
-        pprint(solve_part1(file_input))
+        res = solve_part1(grid)
     else:
-        print(solve_part2(file_input))
+        res = solve_part2(grid)
     end = time.time()
+    print(res)
     print(f"Time: {round((end - start)*1000, 4)}ms")
     
 
