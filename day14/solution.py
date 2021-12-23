064dc21230c98bdf524e18b32452cab71d4f0fb1
SabinaSerra <Sabina.serra@cygni.se>
2021-12-23 20:26:11 +0100

    Clean up

--- day14/solution.py
+++ day14/solution.py
@@ -3,44 +3,28 @@ import time
 from collections import defaultdict
 
 
-def solve_part1(polymer: str, rules: dict[str, str]):
-    res = defaultdict(lambda: 0)
-    for i in range(len(polymer) - 1):
-        rec_sol(polymer[i:i+2], rules, res, 10)
-    print(max(res.values()) - min(res.values()))
-
-
-def rec_sol(polymer: str, rules: dict[str, str], res: dict[str, int], count: int):
-    if count <= 0 or polymer not in rules:
-        return
-    new_letter = rules[polymer]
-    res[new_letter] += 1
-    rec_sol(polymer[0] + new_letter, rules, res, count - 1)
-    rec_sol(new_letter + polymer[1], rules, res, count - 1)
-
-
-def calc(letter_count: dict[str, int], curr_pairs: dict[str, int], rules: dict[str, str]):
+def calc_new_elements(element_counts: dict[str, int], curr_pairs: dict[str, int], rules: dict[str, str]):
     next_pairs = defaultdict(lambda:0)
     for rule in rules:
         if rule in curr_pairs:
             new_letter = rules[rule]
             count = curr_pairs.pop(rule)
-            letter_count[new_letter] += count
+            element_counts[new_letter] += count
             next_pairs[rule[0] + new_letter] += count
             next_pairs[new_letter + rule[1]] += count
     return next_pairs
 
     
-def solve_part2(polymer: str, rules: dict[str: str]):
-    letter_counts = defaultdict(lambda: 0)
+def solve_parts(polymer: str, rules: dict[str: str], steps):
+    element_counts = defaultdict(lambda: 0)
     curr_pairs = defaultdict(lambda: 0)
-    letter_counts[polymer[-1]] += 1
+    element_counts[polymer[-1]] += 1
     for i in range(len(polymer) - 1):
-        letter_counts[polymer[i]] += 1
+        element_counts[polymer[i]] += 1
         curr_pairs[polymer[i:i+2]] += 1
-    for i in range(10):
-        curr_pairs = calc(letter_counts, curr_pairs, rules)
-    print(max(letter_counts.values()) - min(letter_counts.values()))
+    for i in range(steps):
+        curr_pairs = calc_new_elements(element_counts, curr_pairs, rules)
+    print(max(element_counts.values()) - min(element_counts.values()))
 
     
         
@@ -59,9 +43,9 @@ def main():
     start = time.time()
     polymer, rules = parse_input('input.txt')
     if part == 'part1':
-        res = solve_part1(polymer, rules)
+        res = solve_parts(polymer, rules, 10)
     else:
-        res = solve_part2(polymer, rules)
+        res = solve_parts(polymer, rules, 40)
     end = time.time()
     print(res)
     print(f"Time: {round((end - start)*1000, 4)}ms")
